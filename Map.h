c62ece35cfc6e5922b4361c4ab76c1b7b912439d
Proyecto II <sofia.xie.x@gmail.com>
2025-05-08 10:25:27 -0600

    Base del Mapa de juego, implementación básica del algoritmo A*, visualización del grid en la interfaz

---
+++ Map.h
@@ -0,0 +1,40 @@
+#ifndef MAP_H
+#define MAP_H
+#define ROW 15 // Definir tamaño de filas, contantes
+#define COL 15 // Definir tamaño de columnas, constantes
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Map {
+private:
+    typedef pair<int,int> Pair; //
+    typedef pair<double,pair<int,int>> pPairs; //
+    struct cell {
+        int parent_i; // Padre de x
+        int parent_j; // Padre de y
+
+        // Variables para formula calculo de f=g+h
+        double f;
+        double g;
+        double h;
+    };
+public:
+    Map() {} // Constructor
+    // Revisa si la celda es valido o no
+    bool isValid(int row, int col);
+    // Revisa si hay algun obstaculo en el mapa
+    bool isUnBlocked(int grid[][COL],int row, int col);
+    // Revisa si ya llego al objetivo
+    bool isDestination(int row, int col, Pair dest);
+    // Funcion que calcula heuristic
+    double calculateHValue(int row, int col, Pair dest);
+    // Funcion de trazar la ruta
+    void tracePath(cell cellDetails[][COL], Pair dest);
+    // Funcion del Algoritmo A*
+    void aEstrellita(int grid[][COL], Pair scr, Pair dest);
+};
+
+
+
+#endif //MAP_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include "Map.h"
+#include <utility>
+using Pair = std::pair<int, int>;
+using namespace std;
+// Tamaño de celda
+const int SIZE = 40;
 
 int main() {
     // Crear una ventana de 800x600
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Tower Defense");
+    sf::RenderWindow window(sf::VideoMode(COL * SIZE, ROW * SIZE), "Tower Defense");
 
+    // Iniciar mapa
+    int grid[ROW][COL] = { 0 };
+
+    // Rellenarlo con 1
+    for (int i = 0; i < ROW; ++i)
+        for (int j = 0; j < COL; ++j)
+            grid[i][j] = 1; //
+    Map mapa;
+    // Celda de inicio
+    Pair src = make_pair(8, 0);
+    // Celda de fin
+    Pair dest = make_pair(0, 0);
+    // Usar clases mapa
+
+    mapa.aEstrellita(grid,src, dest);
     // Bucle principal del juego
     while (window.isOpen()) { // es como mainloop en tkinter
         sf::Event event;
@@ -12,7 +33,23 @@ int main() {
                 window.close();
         }
 
-        window.clear(sf::Color::Black); // Limpiar la ventana con color negro
+        window.clear()  ; // Limpiar la ventana con color negro
+        // Dibujar celdas
+
+        for (int i = 0; i < ROW; ++i) {
+            for (int j = 0; j < COL; ++j) {
+                sf::RectangleShape cell(sf::Vector2f(SIZE - 1, SIZE - 1));
+                cell.setPosition(j * SIZE, i * SIZE);
+
+                if (grid[i][j] == 0)
+                    cell.setFillColor(sf::Color::Black);         // Ruta encontrada
+                else
+                    cell.setFillColor(sf::Color(220, 220, 220)); // Libre
+
+                window.draw(cell);
+            }
+        }
+
         window.display();               // Mostrar la ventana
     }
 
