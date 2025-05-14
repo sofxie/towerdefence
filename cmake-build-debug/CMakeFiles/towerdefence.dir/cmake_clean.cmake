780e51072a6dc6f337909befb31a1c440a3c9b81
Asrai04 <a.saborio.1@estudiantec.cr>
2025-05-13 18:48:26 -0600

    Funcionalidades del mapa

---
+++ cmake-build-debug/CMakeFiles/towerdefence.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/towerdefence.dir/Map.cpp.obj"
+  "CMakeFiles/towerdefence.dir/Map.cpp.obj.d"
+  "CMakeFiles/towerdefence.dir/main.cpp.obj"
+  "CMakeFiles/towerdefence.dir/main.cpp.obj.d"
+  "libtowerdefence.dll.a"
+  "towerdefence.exe"
+  "towerdefence.exe.manifest"
+  "towerdefence.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/towerdefence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
